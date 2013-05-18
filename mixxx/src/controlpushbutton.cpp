421b9c9a2a7942fbcbd26882603973a2fb9da22b
Owen Williams <owilliams@mixxx.org>
2013-05-18 19:00:17 -0400

    unify with trunk

--- mixxx/src/controlpushbutton.cpp
+++ mixxx/src/controlpushbutton.cpp
@@ -54,51 +54,12 @@ void ControlPushButton::setButtonMode(enum ButtonMode mode) {
 void ControlPushButton::setStates(int num_states) {
     m_iNoStates = num_states;
 
-void ControlPushButton::setValueFromMidi(MidiOpCode o, double v) {
-    // keyboard events are handled by this function as well
-    //if (m_bMidiSimulateLatching)
-
-    //qDebug() << "bMidiSimulateLatching is true!";
-    // Only react on NOTE_ON midi events if simulating latching...
-
-    //qDebug() << o << v;
-
-    // This block makes push-buttons act as power window buttons.
-    if (m_buttonMode == POWERWINDOW && m_iNoStates == 2) {
-        if (o == MIDI_NOTE_ON) {
-            if (v > 0.) {
-                m_dValue = !m_dValue;
-                m_pushTimer.setSingleShot(true);
-                m_pushTimer.start(kPowerWindowTimeMillis);
-            }
-        } else if (o == MIDI_NOTE_OFF) {
-            if (!m_pushTimer.isActive()) {
-                m_dValue = 0.0;
-            }
-        }
-    } else if (m_buttonMode == TOGGLE) {
-        // This block makes push-buttons act as toggle buttons.
-        if (m_iNoStates > 2) { //multistate button
-            if (v > 0.) { //looking for NOTE_ON doesn't seem to work...
-                m_dValue++;
-                if (m_dValue >= m_iNoStates)
-                    m_dValue = 0;
-            }
-        } else {
-            if (o == MIDI_NOTE_ON) {
-                if (v > 0.) {
-                    m_dValue = !m_dValue;
-                }
-            }
-        }
-    } else { //Not a toggle button (trigger only when button pushed)
-        if (o == MIDI_NOTE_ON) {
-            m_dValue = !m_dValue;
-        } else if (o == MIDI_NOTE_OFF) {
-            m_dValue = 0.0;
-        }
+    if (m_pControl) {
+        ControlNumericBehavior* pOldBehavior = m_pControl->setBehavior(
+            new ControlPushButtonBehavior(
+                static_cast<ControlPushButtonBehavior::ButtonMode>(m_buttonMode),
+                m_iNoStates));
+        delete pOldBehavior;
     }
-
-    emit(valueChanged(m_dValue));
 }
 
