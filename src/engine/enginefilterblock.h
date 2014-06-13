b90fffbb525c62958d9a108dd27630cdf66b1030
Owen Williams <owilliams@mixxx.org>
2014-06-12 21:32:44 -0400

    Revert EQ optimization code, which was producing strange-sounding effects

--- src/engine/enginefilterblock.h
+++ src/engine/enginefilterblock.h
@@ -47,7 +47,7 @@ class EngineFilterBlock : public EngineObject {
   private:
     void setFilters(bool forceSetting = false);
 
-    CSAMPLE *m_pLowBuf, *m_pBandBuf, *m_pHighBuf;
+    CSAMPLE *m_pTemp1, *m_pTemp2, *m_pTemp3;
     EngineObjectConstIn *low, *band, *high;
     ControlLogpotmeter *filterpotLow, *filterpotMid, *filterpotHigh;
     ControlPushButton *filterKillLow, *filterKillMid, *filterKillHigh;
@@ -58,7 +58,7 @@ class EngineFilterBlock : public EngineObject {
     static ControlPushButton *s_EnableEq;
 
     int m_iOldSampleRate;
-    double old_low, old_mid, old_high, old_dry;
+    double old_low, old_mid, old_high;
 
     int ilowFreq, ihighFreq;
     bool blofi;
