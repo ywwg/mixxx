5c75ff882d6599e94694d0d52983e64520855896
Owen Williams <owilliams@mixxx.org>
2014-12-28 10:55:03 -0500

    The default code was messing with my defaults

--- src/skin/legacyskinparser.cpp
+++ src/skin/legacyskinparser.cpp
@@ -118,22 +118,9 @@ ControlObject* LegacySkinParser::controlFromConfigNode(QDomElement element,
     QString name = m_pContext->nodeToString(keyElement);
     ConfigKey key = ConfigKey::parseCommaSeparated(name);
 
-    QString default_val = m_pContext->selectAttributeString(keyElement, "default", "0");
     bool bPersist = m_pContext->selectAttributeBool(keyElement, "persist", false);
 
-    ControlObject* co = controlFromConfigKey(key, bPersist, created);
-    if (*created) {
-        bool success = false;
-        double d = default_val.toDouble(&success);
-        if (success) {
-            co->set(static_cast<double>(d));
-        } else if (default_val.toLower() == "true") {
-            co->set(1.0);
-        } else if (default_val.toLower() == "false") {
-            co->set(0.0);
-        }
-    }
-    return co;
+    return controlFromConfigKey(key, bPersist, created);
 }
 
 LegacySkinParser::LegacySkinParser(ConfigObject<ConfigValue>* pConfig,
