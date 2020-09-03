616f664d945bc8a6e1a6b96634d329181af4cd03
Owen Williams <owilliams@mixxx.org>
2020-09-03 16:02:22 -0400

    Rebasing

--- src/test/signalpathtest.h
+++ src/test/signalpathtest.h
@@ -90,10 +90,18 @@ class BaseSignalPathTest : public MixxxTest {
                 m_pVisualsManager,
                 EngineChannel::CENTER,
                 m_pEngineMaster->registerChannelGroup(m_sGroup3));
+        m_pMixerDeck4 = new Deck(NULL,
+                m_pConfig,
+                m_pEngineMaster,
+                m_pEffectsManager,
+                m_pVisualsManager,
+                EngineChannel::CENTER,
+                m_sGroup4);
 
         m_pChannel1 = m_pMixerDeck1->getEngineDeck();
         m_pChannel2 = m_pMixerDeck2->getEngineDeck();
         m_pChannel3 = m_pMixerDeck3->getEngineDeck();
+        m_pChannel4 = m_pMixerDeck4->getEngineDeck();
         m_pPreview1 = new PreviewDeck(nullptr,
                 m_pConfig,
                 m_pEngineMaster,
@@ -113,6 +121,7 @@ class BaseSignalPathTest : public MixxxTest {
         addDeck(m_pChannel1);
         addDeck(m_pChannel2);
         addDeck(m_pChannel3);
+        addDeck(m_pChannel4);
 
         m_pEngineSync = m_pEngineMaster->getEngineSync();
         ControlObject::set(ConfigKey(m_sMasterGroup, "enabled"), 1.0);
@@ -124,9 +133,11 @@ class BaseSignalPathTest : public MixxxTest {
         delete m_pMixerDeck1;
         delete m_pMixerDeck2;
         delete m_pMixerDeck3;
+        delete m_pMixerDeck4;
         m_pChannel1 = NULL;
         m_pChannel2 = NULL;
         m_pChannel3 = NULL;
+        m_pChannel4 = NULL;
         m_pEngineSync = NULL;
         delete m_pPreview1;
 
@@ -231,8 +242,8 @@ class BaseSignalPathTest : public MixxxTest {
     EffectsManager* m_pEffectsManager;
     EngineSync* m_pEngineSync;
     TestEngineMaster* m_pEngineMaster;
-    Deck *m_pMixerDeck1, *m_pMixerDeck2, *m_pMixerDeck3;
-    EngineDeck *m_pChannel1, *m_pChannel2, *m_pChannel3;
+    Deck *m_pMixerDeck1, *m_pMixerDeck2, *m_pMixerDeck3, *m_pMixerDeck4;
+    EngineDeck *m_pChannel1, *m_pChannel2, *m_pChannel3, *m_pChannel4;
     PreviewDeck* m_pPreview1;
 
     static const QString m_sMasterGroup;
@@ -240,6 +251,7 @@ class BaseSignalPathTest : public MixxxTest {
     static const QString m_sGroup1;
     static const QString m_sGroup2;
     static const QString m_sGroup3;
+    static const QString m_sGroup4;
     static const QString m_sPreviewGroup;
     static const QString m_sSamplerGroup;
     static const double kDefaultRateRange;
