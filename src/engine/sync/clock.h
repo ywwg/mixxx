9d6ea2a9e26d30ac81d4858d8e8216ac015fe473
Owen Williams <owilliams@mixxx.org>
2021-05-28 16:34:16 -0400

    Rename Sync instances of "master" to "leader"
    Also add an alias from the sync_master CO to sync_leader

--- src/engine/sync/clock.h
+++ src/engine/sync/clock.h
@@ -5,8 +5,8 @@ class Clock {
     virtual ~Clock() = default;
 
     virtual double getBeatDistance() const = 0;
-    virtual void setMasterBeatDistance(double beatDistance) = 0;
+    virtual void setLeaderBeatDistance(double beatDistance) = 0;
 
     virtual double getBpm() const = 0;
-    virtual void setMasterBpm(double bpm) = 0;
+    virtual void setLeaderBpm(double bpm) = 0;
 };
